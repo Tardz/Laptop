036b97578ba631f104d8daa777035664c7d02698
Tardz <jonathan.almstedt@live.se>
2023-03-28 19:49:32 +0200

    commit 6

--- scripts/rofi/automation/save_files.sh
+++ scripts/rofi/automation/save_files.sh
@@ -2,18 +2,20 @@
 
 git_path="/home/jonalm/laptopgit/Laptop"
 json_file="/home/jonalm/scripts/rofi/automation/save_files_data.json"
-current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
-old_date=$(date "+%Y-%m-%d %H:%M:%S")
 
 echo "$current_date_time"
 if test -f "$json_file"; then
-  old_date=$(jq '.old_date' "$json_file")
+  old_date=$(jq '.date' "$json_file")
   old_number=$(jq '.number' "$json_file")
   new_number=$((old_number + 1))
+  echo "$old_date"
+  echo "$old_number"
 else
   new_number=1
-  echo "{ \"number\": $new_number, \"date\": \"$current_date_time\, \"oldDate\": \"$old_date\" }" | jq . > "$json_file"
+  echo "{ \"number\": $new_number, \"date\": \"$current_date_time\", \"old_date\": \"$old_date\" }" | jq . > "$json_file"
 fi
+
+current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
 
 sudo cp -r /home/jonalm/laptopgit/Laptop/ /home/jonalm/laptopgit/LaptopBackup/
 sudo cp -r /home/jonalm/scripts "$git_path"
@@ -23,7 +25,6 @@ sudo cp -r /home/jonalm/.config/alacritty "$git_path"
 sudo cp -r /home/jonalm/.config/picom.conf "$git_path"
 sudo cp -r /home/jonalm/.imwheelrc "$git_path"
 sudo cp -r /home/jonalm/.inputrc "$git_path"
-sudo cp -r /home/jonalm/.doom.d "$git_path"
 
 cd /home/jonalm/laptopgit/Laptop/
 git add --all
@@ -31,7 +32,7 @@ git commit -m "commit ${new_number}"
 git push -u -f origin main
 
 if [ $? -eq 0 ]; then
-  echo "{ \"number\": $new_number, \"date\": \"$current_date_time\, \"old_date\": \"$old_date\" }" | jq . > "$json_file"
+  echo "{ \"number\": $new_number, \"date\": \"$current_date_time\", \"old_date\": \"$old_date\" }" | jq . > "$json_file"
   echo "Commit successful!"
 else
   echo "Commit failed!"
