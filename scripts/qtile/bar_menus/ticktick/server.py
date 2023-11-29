bf7b24b30019a574742c978bed6d815ad49dffb5
Tardz <jonathan.almstedt@live.se>
2023-11-29 20:02:26 +0100

    commit 88

--- scripts/qtile/bar_menus/ticktick/server.py
+++ scripts/qtile/bar_menus/ticktick/server.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_caching import Cache
 from requests_oauthlib import OAuth2Session
 from requests.exceptions import HTTPError
+import time
 
 import os 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__)
 app.secret_key = 'awdnwudop8hoiwn1iuhe027887ocjnkl'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
@@ -18,30 +19,54 @@ client_id = 'J7jnDtoFyf501eepIw'
 client_secret = 'J8c@FHmETp9(MBh#_)c46j9#_Ike1cDP'
 authorization_base_url = 'https://ticktick.com/oauth/authorize'
 token_url = 'https://ticktick.com/oauth/token'
+base_url = 'https://api.ticktick.com/open/v1/'
 redirect_uri = 'https://0d39-155-4-149-181.ngrok-free.app/callback'
 scope = ['tasks:read', 'tasks:write']
 
-class Task(db.Model):
+class Token(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
+    access_token = db.Column(db.String(255), nullable=False)
+    expires_at = db.Column(db.Float, nullable=False)
+    expires_in = db.Column(db.Float, nullable=False)
 
-@app.route('/')
-def home():
-    return 'Hello, this is your local server!'
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    column_id = db.Column(db.Float, nullable=True)
+    content = db.Column(db.String(255), nullable=True)
+    desc = db.Column(db.String(255), nullable=True)
+    due_date = db.Column(db.String(255), nullable=True)
+    task_id = db.Column(db.String(255), nullable=False)
+    is_all_day = db.Column(db.String(255), nullable=False)
+    priority = db.Column(db.Float, nullable=False)
+    project_id = db.Column(db.Float, nullable=False)
+    repeat_flag = db.Column(db.String(255), nullable=True)
+    sort_order = db.Column(db.Float, nullable=False)
+    start_date = db.Column(db.String(255), nullable=True)
+    status = db.Column(db.Float, nullable=False)
+    time_zone = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255), nullable=False)
+    linked_to_project = db.relationship('Projects', backref='project', lazy=True)
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    color = db.Column(db.String(20), nullable=False)
+    group_id = db.Column(db.String(255), nullable=False)
+    item_id = db.Column(db.String(255), nullable=False)
+    kind = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    sort_order = db.Column(db.Float, nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))  # Adjust ForeignKey to reference task_dir.id
 
 @app.route('/login')
 def login():
     ticktick = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
     authorization_url, state = ticktick.authorization_url(authorization_base_url)
     
-    print(cache.get('oauth_state'))
-    
-    if not cache.get('oauth_state'):
+    if not Token.query.first():
         cache.set('oauth_state', state)
         return redirect(authorization_url)
     else:
-        return "Already verified!"
+        return jsonify({"result": "oauth token active"})
 
 @app.route('/callback')
 def callback():
@@ -57,32 +82,67 @@ def callback():
         client_secret=client_secret
     )
 
-    print(token)
-
-    cache.set('oauth_token', token)
+    stored_token = Token(
+        access_token=token['access_token'],
+        expires_at=token['expires_at'],
+        expires_in=token['expires_in']
+    )
+    
+    db.session.add(stored_token)
+    db.session.commit()
 
     return 'Authorization successful!'
 
-@app.route('/tasks')
-def get_tasks():
-    token = cache.get('oauth_token')
-    
-    if not token:
-        return redirect(url_for('login'))
+@app.route('/projects')
+def get_projects():
+    token = Token.query.first()
 
-    ticktick = OAuth2Session(client_id, token=token)
+    if not token or token.expires_at < time.time():
+        return redirect(url_for('login'))
+    
+    token_dict = {
+        'access_token': token.access_token,
+        'expires_at': token.expires_at,
+        'expires_in': token.expires_in
+    }
+    
+    ticktick = OAuth2Session(client_id, token=token_dict)
     
     try:
-        # Make a request to TickTick API to get tasks
-        response = ticktick.get('https://api.ticktick.com/open/v1/project')
+        response = ticktick.get(f'{base_url}project')
         response.raise_for_status()
         tasks = response.json()
-        print(jsonify(tasks))
         return jsonify(tasks)
+    
+    except HTTPError as e:
+        return f'Error accessing TickTick API: {e.response.text}', 500
+    
+@app.route('/project')
+def get_project():
+    token = Token.query.first()
+
+    if not token or token.expires_at < time.time():
+        return redirect(url_for('login'))
+    
+    token_dict = {
+        'access_token': token.access_token,
+        'expires_at': token.expires_at,
+        'expires_in': token.expires_in
+    }
+    
+    ticktick = OAuth2Session(client_id, token=token_dict)
+    
+    try:
+        project_id = request.args.get("project_id")
+        response = ticktick.get(f'{base_url}project/{project_id}/data')
+        response.raise_for_status()
+        tasks = response.json()
+        return jsonify(tasks)
+    
     except HTTPError as e:
         return f'Error accessing TickTick API: {e.response.text}', 500
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all() 
-    app.run(host='0.0.0.0', port=5000)
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- scripts/qtile/bar_menus/ticktick/ticktick_menu.py
+++ scripts/qtile/bar_menus/ticktick/ticktick_menu.py
@@ -2,25 +2,120 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk, WebKit2
-import jsonify
+from gi.repository import Gtk, Gdk, GLib, Pango
+import requests
 
 class TicktickMenu(Gtk.Dialog):
     def __init__(self):
-        Gtk.Dialog.__init__(self, "OAuth2 Authorization", None, 0)
+        Gtk.Dialog.__init__(self, "TickTick Menu", None, 0)
 
-        content_area = self.get_content_area()
+        self.set_size_request(400, 500)
+
+        self.content_area = self.get_content_area()
         self.webview = WebKit2.WebView()
+
+        self.server_base_url = 'http://127.0.0.1:5000/'
+
+        self.list()
+        self.css()
+
+        self.content_area.pack_start(self.list_main_box, True, True, 0)
+        
         content_area.pack_start(self.webview, True, True, 0)
-
         self.webview.connect("load-changed", self.on_load_changed)
-
         authorization_url = "http://127.0.0.1:5000/login"  # Replace with your server's IP and port
-
         self.webview.load_uri(authorization_url)
-
-        self.set_size_request(800, 600)
-
+        # self.fetch_projects()
+        # self.fetch_project({"project_id": "697a4292a33a085e060d7e62"})
         self.show_all()
+
+    def list(self):
+        self.list_main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=16)
+        
+        self.list_box = Gtk.ListBox()
+        self.list_box.set_name("list")
+        self.list_box.set_selection_mode(Gtk.SelectionMode.NONE)
+        
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_name("list-box")
+        scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC) 
+        scrolled_window.add(self.list_box)  
+
+        self.list_main_box.pack_start(scrolled_window, True, True, 0)
+        # self.update_list_with_projects()
+
+    def css(self):
+        screen = Gdk.Screen.get_default()
+        provider = Gtk.CssProvider()
+        style_context = Gtk.StyleContext()
+        style_context.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        provider.load_from_path("/home/jonalm/scripts/qtile/bar_menus/ticktick/ticktick_menu_styles.css")
+        visual = screen.get_rgba_visual()
+        self.content_area.set_visual(visual)
+        self.set_visual(visual)
+
+    def on_project_clicked(self,  widget, event, dir):
+        pass
+
+    def on_project_pressed(self, entry, widget, dir):
+        self.on_project_clicked(widget=widget, dir=dir)
+
+    def update_list_with_projects(self):
+        projects = self.fetch_projects()
+        for project in projects:
+            row = Gtk.ListBoxRow()
+            row.set_name("row")
+
+            list_content_main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+
+            list_content_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+
+            list_obj_icon_box = Gtk.EventBox()
+            list_obj_icon_box.set_name("list-icon-box")
+            icon = Gtk.Label()
+            icon.set_name("list-icon")
+            icon.set_text(project["name"][0])
+            list_obj_icon_box.add(icon)
+
+            list_obj_clickable_box = Gtk.EventBox()
+            list_obj_clickable_box.connect("button-press-event", self.on_project_clicked, project)
+            project_name = Gtk.Label()
+            project_name.set_name("list-obj")
+            project_name.set_text(project["name"][1:])
+            list_obj_clickable_box.add(project_name)
+
+            list_content_box.pack_start(list_obj_icon_box, True, True, 0)
+            list_content_box.pack_start(list_obj_clickable_box, True, True, 0)
+            list_content_main_box.pack_start(list_content_box, True, True, 0)
+
+            row.add(list_content_main_box)
+            row.connect("activate", self.on_project_pressed, list_obj_clickable_box, project)
+            self.list_box.add(row)
+
+        self.list_box.show_all()
+        return True
+
+    def fetch_projects(self):
+        response = requests.get(f'{self.server_base_url}projects')
+
+        if response.status_code == 200:
+            projects = response.json()
+            print(projects)
+            return projects
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            return []
+        
+    def fetch_project(self, project_id):
+        response = requests.get(f'{self.server_base_url}project', params=project_id)
+
+        if response.status_code == 200:
+            tasks = response.json()
+            print(tasks)
+            return tasks
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            return []
 
     def on_load_changed(self, webview, event):
         # Check if the page has finished loading
@@ -29,20 +124,7 @@ class TicktickMenu(Gtk.Dialog):
             print(f"Current URI: {current_uri}")
 
             if "login" in current_uri:
-                self.fetch_tasks()
-
-    def fetch_tasks(self):
-        import requests
-
-        response = requests.get('http://127.0.0.1:5000/tasks')
-
-        if response.status_code == 200:
-            tasks = response.json()
-            for task in tasks:
-                print(task, "\n")
-            # Process tasks as needed in your GTK app
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
+                self.fetch_projects()
 
 if __name__ == '__main__':
     win = TicktickMenu()
